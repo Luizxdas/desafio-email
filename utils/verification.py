cc85eb9012b0553f0299f89af5f1f3c7929bde25
Luiz <148092584+Luizxdas@users.noreply.github.com>
2025-09-30 17:38:53 -0300

    Alterações no tamanho de caracteres
    - Aumenta tamanho de caracteres permitidos no textarea
    - Adiciona verificação para número de caracteres em arquivos

--- utils/verification.py
+++ utils/verification.py
@@ -8,15 +8,20 @@ def verify_and_read(file):
     file.seek(0)
     if file_size > MAX_FILE_SIZE:
         raise ValueError("Arquivo muito grande")
+    
+    conteudo = ""
 
     if file.filename.endswith(".txt") and file.mimetype == "text/plain":
         conteudo = file.read().decode("utf-8")
-        return conteudo
     elif file.filename.endswith(".pdf") and file.mimetype == "application/pdf":
         reader = PdfReader(file)
-        texto = ""
         for page in reader.pages:
-            texto += page.extract_text() or ""
-        return texto
+            conteudo += page.extract_text() or ""
     else:
         raise ValueError("Apenas arquivos .txt ou .pdf são permitidos")
+    
+    tamanho = len(conteudo)
+    if tamanho > 800:
+        raise ValueError("O arquivo excede o limite de 800 caracteres.")
+    
+    return conteudo
